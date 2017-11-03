51112f587c14aeb776718bc99cca895e2859fbc1
Hyago Souza <hyagosouzza@hotmail.com>
2017-11-03 11:47:09 -0200

    Adicionando links e disable

--- frontend/controllers/mainController.js
+++ frontend/controllers/mainController.js
@@ -1,4 +1,7 @@
 angular.module('meuApp')
     .controller('mainController', ['$scope', '$http', '$location', '$routeParams', function($scope, $http, $location, $routeParams){
-        $scope.data = new Date();
+        $scope.data = new Date(); 
+        $scope.admins = [
+            {email : "hyagosouzza@hotmail.com", senha : "123"}
+        ];
     }]);
--- server.js
+++ server.js
@@ -3,13 +3,19 @@ var server = express();
 var bodyparser = require('body-parser');
 var mongoose = require('mongoose');
 
+server.use(bodyparser());
+
+server.post('/api/login', function(req, res){
+    var login = req.body;
+    
+});
+
 Admin = require('./models/admin.js');
 Vaga = require('./models/vaga.js');
 
 // Conect to mongoose
 mongoose.connect('mongodb://localhost');
 var db = mongoose.connection;
-
 
 server.use(express.static(__dirname + '/frontend'));
 
