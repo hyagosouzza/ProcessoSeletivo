f30f4b06ee7d071870c4e021916da569fd9b6fe4
João Pedro Pinheiro <jprnp18@gmail.com>
2017-11-07 21:22:59 -0200

    Nova versão da função de validação de admin

--- frontend/controllers/mainController.js
+++ frontend/controllers/mainController.js
@@ -1,19 +1,27 @@
 angular.module('meuApp')
     .controller('mainController', ['$scope', '$http', '$location', '$routeParams', function ($scope, $http, $location, $routeParams) {
         $scope.data = new Date();
-        $scope.admins = [
-            { email: "hyago@hotmail.com", senha: "123" },
-            { email: "joaop@gmail.com", senha: "123" },
-            { email: "pedroh@hotmail.com", senha: "123" },
-            { email: "estevao@hotmail.com", senha: "123" }
-        ];
-        $scope.verificaAdmin = function (email, senha) {
-            var index = 0;
-            for (index = 0; index < $scope.admins.length; index++) {
-                if (email == $scope.admins[index].email && senha == $scope.admins[index].senha) {
-                    return window.location.href = "#!/admin";
-                }
+        encrypt = function (string) {
+            encrypted = 0;
+            for (i = 0; i < string.length; i++) {
+                encrypted  += string.charCodeAt(i);
             }
-            return alert("Email ou Senha incorretos!");
+            encrypted *= string.length;
+            encrypted = encrypted.toString(16);
+            return encrypted;
+        }
+
+        $scope.verificaAdmin = function (email, senha) {
+            $.ajax ({
+                url: "https://api.mlab.com/api/1/databases/selecao_de_vagas/collections/Admins?apiKey=uUgrdYIhiMZ1vRAdaUTJcHRtpBhwefCk",
+            }).done(function (data) {
+                var index = 0;
+                for (index = 0; index < data.length; index++) {
+                    if (email == data[index].email && encrypt(senha) == data[index].senha) {
+                        return window.location.href = "#!/admin";
+                    }
+                }
+                return alert("Email ou Senha incorretos!");
+            });
         };
     }]);
