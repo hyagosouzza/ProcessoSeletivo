ae72e0106e93bfee363f78798af596468015a45b
João Pedro Pinheiro <jprnp18@gmail.com>
2017-11-08 11:05:38 -0200

    Autenticação via server

--- frontend/controllers/mainController.js
+++ frontend/controllers/mainController.js
@@ -1,27 +1,22 @@
 angular.module('meuApp')
     .controller('mainController', ['$scope', '$http', '$location', '$routeParams', function ($scope, $http, $location, $routeParams) {
         $scope.data = new Date();
-        encrypt = function (string) {
-            encrypted = 0;
-            for (i = 0; i < string.length; i++) {
-                encrypted  += string.charCodeAt(i);
-            }
-            encrypted *= string.length;
-            encrypted = encrypted.toString(16);
-            return encrypted;
-        }
+
+        var socket = io.connect('http://127.0.0.1:8000');
+
+        socket.on('authenticated', function() {
+            return window.location.href = "#!/admin";
+        });
+
+        socket.on('notAuthenticated', function() {
+            return alert("Email ou Senha incorretos!");
+        });
 
         $scope.verificaAdmin = function (email, senha) {
             $.ajax ({
                 url: "https://api.mlab.com/api/1/databases/selecao_de_vagas/collections/Admins?apiKey=uUgrdYIhiMZ1vRAdaUTJcHRtpBhwefCk",
-            }).done(function (data) {
-                var index = 0;
-                for (index = 0; index < data.length; index++) {
-                    if (email == data[index].email && encrypt(senha) == data[index].senha) {
-                        return window.location.href = "#!/admin";
-                    }
-                }
-                return alert("Email ou Senha incorretos!");
+            }).done(function (lista) {
+                socket.emit('getAdmins', JSON.stringify({"lista" : lista, "email" : email, "senha" : senha}));
             });
         };
     }]);
--- server.js
+++ server.js
@@ -1,9 +1,43 @@
 var express = require('express');
-var server = express();
+var app = express();
+var server = require('http').createServer(app);
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var io = require('socket.io')(server);
 
-server.use(express.static(__dirname + '/frontend'));
+app.use(express.static(__dirname + '/frontend'));
+
+encrypt = function (string) {
+    encrypted = 0;
+    for (i = 0; i < string.length; i++) {
+        encrypted  += string.charCodeAt(i);
+    }
+    encrypted *= string.length;
+    encrypted = encrypted.toString(16);
+    return encrypted;
+};
+
+io.on('connection', function (client) {
+    console.log("Client connected");
+
+    client.on('getAdmins', function (data) {
+        console.log("getAdmins");
+        obj = JSON.parse(data);
+
+        var index = 0;
+        var isAdmin = false;
+
+        for (index = 0; index < obj["lista"].length; index++) {
+            if (obj["email"] == obj["lista"][index].email && encrypt(obj["senha"]) == obj["lista"][index].senha) {
+                isAdmin = true;
+                io.sockets.emit("authenticated");
+            }
+        }
+        if (!isAdmin) {
+            io.sockets.emit("notAuthenticated");
+        }
+    })
+})
 
 var porta = process.env.PORT || 8000;
 server.listen(porta, function(){
